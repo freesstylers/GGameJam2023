c1cf3fb1e7ebdd2b70a948c96baa71aa053f1e16
IgnOry <iory@ucm.es>
2023-02-04 08:24:47 +0100

    Shop started

--- Assets/Scripts/UI/Shop.cs
+++ Assets/Scripts/UI/Shop.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
@@ -8,16 +9,46 @@ public class Shop : MonoBehaviour
     Transform container;
     [SerializeField]
     Transform itemTemplate;
+    [SerializeField]
+    float ItemHeight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateItemButton(Item.ItemType.ExtraSword, Item.getItemCost(Item.ItemType.ExtraSword), 0);   
+        CreateItemButton(Item.ItemType.UpdgradeSword, Item.getItemCost(Item.ItemType.UpdgradeSword), 1);   
+        CreateItemButton(Item.ItemType.ExtraHealth, Item.getItemCost(Item.ItemType.ExtraHealth), 2);   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void CreateItemButton(Item.ItemType type, int itemCost, int positionIndex)
+    {
+        Transform additionalItem = Instantiate(itemTemplate, container);
+
+        RectTransform additionalItemRT = additionalItem.GetComponent<RectTransform>();
+
+        additionalItemRT.anchoredPosition = new Vector2(0, -ItemHeight * positionIndex);
+
+        //Set template parameters
+    }
+
+    public void BuyItem(Item.ItemType type)
+    {
+        //Check money
+
+        //if more than item.cost
+        //{
+        //add item to player
+        //remove from shop
+        //}
+        //else
+        //{
+
+        //}
     }
 
     public class Item
@@ -25,7 +56,9 @@ public class Shop : MonoBehaviour
         public enum ItemType
         {
             None,
-            ExtraSword
+            ExtraSword,
+            UpdgradeSword,
+            ExtraHealth
             //...
         }
 
@@ -40,6 +73,12 @@ public class Shop : MonoBehaviour
                     break;
                 case ItemType.ExtraSword:
                     ret = 100;
+                    break;
+                case ItemType.UpdgradeSword:
+                    ret = 200;
+                    break;
+                case ItemType.ExtraHealth:
+                    ret = 150;
                     break;
             }
 
