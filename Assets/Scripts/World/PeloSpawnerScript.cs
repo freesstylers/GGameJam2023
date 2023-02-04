70b8a6b65e62e6692805e24120ae10a8f13730bc
GCidon <gcidonch@ucm.es>
2023-02-04 10:59:32 +0100

    Generacion de pelos ordenaditos

--- Assets/Scripts/World/PeloSpawnerScript.cs
+++ Assets/Scripts/World/PeloSpawnerScript.cs
@@ -11,6 +11,8 @@ public class PeloSpawnerScript : MonoBehaviour
     public int minPelos_;
     public int maxPelos_;
 
+    public int peloDistance;
+
     void Start()
     {
         RectTransform tr_ = GetComponent<RectTransform>();
@@ -18,21 +20,45 @@ public class PeloSpawnerScript : MonoBehaviour
         float width = tr_.rect.width;
         float height = tr_.rect.height;
 
-        float iniPosX = tr_.rect.position.x;
-        float iniPosY = tr_.rect.position.y;
+        float iniPosX = tr_.position.x-width/2;
+        float iniPosY = tr_.position.y-height/2;
 
-        int cantPelosFinal = Mathf.RoundToInt((maxPelos_ - minPelos_) * density_);
+        int cantPelosFinal = Mathf.RoundToInt((maxPelos_ - minPelos_) * (density_ + Random.Range(-density_/10, density_/10))) + minPelos_;
+
+        List<Transform> pelosPositionsList = new List<Transform>();
 
         for(int pelo = 0; pelo < cantPelosFinal; pelo++)
         {
             GameObject thisPelo = Instantiate(pelo_);
             Vector2 thisPeloPos = new Vector2();
 
-            thisPeloPos.x = Random.Range(iniPosX, iniPosX+width);
-            thisPeloPos.y = Random.Range(iniPosY, iniPosY+height);
+            bool safeZoned = false;
+            while (!safeZoned)
+            {
+                safeZoned = true;
+
+                thisPeloPos.x = Random.Range(iniPosX, iniPosX + width);
+                thisPeloPos.y = Random.Range(iniPosY, iniPosY + height);
+
+                foreach (Transform pos in pelosPositionsList)
+                {
+                    if (Vector2.Distance(thisPeloPos, pos.position) < peloDistance) safeZoned = false;
+                }
+            }
 
             thisPelo.transform.position = thisPeloPos;
-            thisPelo.transform.parent = tr_;
+            pelosPositionsList.Add(thisPelo.transform);
+
+            thisPelo.transform.parent = null;
+        }
+
+        pelosPositionsList.Sort((Transform t1, Transform t2) => { return t1.position.y.CompareTo(t2.position.y); });
+
+        for(int i = 0; i < pelosPositionsList.Count; i++)
+        {
+            Transform aux = pelosPositionsList[i];
+            aux.parent = tr_;
+            aux.GetComponent<SpriteRenderer>().sortingOrder = pelosPositionsList.Count-i;
         }
     }
 
