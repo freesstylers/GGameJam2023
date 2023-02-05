85ae1e4123d40d12b33207110fece80ec86c7b33
javiarias <fezkip@gmail.com>
2023-02-05 11:12:06 +0100

    block botones menú si juego está cargando

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -20,41 +20,63 @@ public class MainMenu : MonoBehaviour
 
     bool settingsOpened = false;
     bool creditsOpened = false;
+    bool playing = false;
+
     // Start is called before the first frame update
     void Start()
     {
         settingsGameObject.SetActive(settingsOpened);
         creditsGameObject.SetActive(creditsOpened);
         EventSystem.current.SetSelectedGameObject(firstSelected);
+        playing = false;
     }
 
     public void Twitter()
     {
+        if(playing)
+            return;
+
         Application.OpenURL("https://twitter.com/FreeStylers_Dev");
     }
 
     public void GGJ()
     {
+        if(playing)
+            return;
+
         Application.OpenURL("https://www.gamejam.es");
     }
 
     public void ItchIO()
     {
+        if(playing)
+            return;
+
         Application.OpenURL("https://freestylers-studio.itch.io/");
     }
 
     public void GlobalGameJam()
     {
+        if (playing)
+            return;
+
         Application.OpenURL("https://globalgamejam.org/");
     }
 
-    public void PlayButton()
+    public void PlayButton(Button b)
     {
+        if (playing)
+            return;
+
+        playing = true;
         GameManager.instance.levelLoader.LoadScene("GameScreen");
     }
 
     public void Settings()
     {
+        if(playing)
+            return;
+
         settingsOpened = !settingsOpened;
 
         if (settingsOpened)
@@ -77,11 +99,17 @@ public class MainMenu : MonoBehaviour
 
     public void HighScores()
     {
+        if(playing)
+            return;
+
 
     }
 
     public void Credits()
     {
+        if(playing)
+            return;
+
         creditsOpened = !creditsOpened;
 
         if (creditsOpened)
